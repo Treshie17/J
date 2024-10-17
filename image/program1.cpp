22d37737050a135a48dc9f0b90fcee5f87f618a4
Treshie17 <cablarajasie17@gmail.com>
2024-10-17 16:11:52 +0800

    Create program1.cpp

---
+++ image/program1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   starting name;
+   cout<<"Welcome to C++ language Programming";
+   cout<<"\nGoodmorning pal!, What is your name?";
+   cin>>jasie;
+   cout<<"hi"jasie",Welcome to C++ Programming Class.\n";
+   }
