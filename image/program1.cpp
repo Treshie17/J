16777010646a00777f1c5b78e48e6c2987f9c543
Jasie Cablara <cablarajasie17@gmail.com>
2024-11-28 14:28:15 +0800

    Update program1.cpp

--- image/program1.cpp
+++ image/program1.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-   starting name;
+	string name;
    cout<<"Welcome to C++ language Programming";
-   cout<<"\nGoodmorning pal!, What is your name?";
-   cin>>jasie;
-   cout<<"hi"jasie",Welcome to C++ Programming Class.\n";
+   cout<<"\nGoodmorning pal!, What is your name? ";
+   cin>>name;
+   cout<<"hi "+ name +" Welcome to C++ Programming Class.\n";
    }
